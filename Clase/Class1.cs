8809db868a9fd61e31f2644baabb4813099a5f96
Stefan <Stefan@DESKTOP-APVQ07C>
2021-04-25 20:21:58 +0300

    Add project files.

---
+++ Clase/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clase
+{
+    public class masina
+    {
+    List<string> Dotari = new List<string>()
+    {
+        "Aer Conditionat","Climatronic","Geamuri electice fata","Geamuri electice spate","Pilot automat",
+        "Pilot automat adaptiv"
+        //... Mai urmeaza
+    };
+    string Fabricant, Model, Echipare;
+    int kilometri, anFabricatie, cilindree, pret;
+    }
+
+    public class client
+    {
+        string Nume, Prenume, adresa, judet;
+        int CNP, venitAnual,buget;
+    }
+}
